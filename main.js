3a2869f4c3d227884e851b7cb9e32391544f6cc2
Kory Hutchison <kory.hutchison@icloud.com>
2018-03-26 09:07:43 -0600

    Set up basic structure

---
+++ main.js
@@ -0,0 +1,47 @@
+/*==============================================================================
+* File:   main.js
+* Author: Kory Hutchison
+* Date:   Winter 2018
+*
+* Description: JavaScript library for the OMDb API
+*              IS 542, Winter 2018, BYU.
+*/
+
+
+const Omdb = (function () {
+    "use strict";
+    /*----------------------------------------------------------------------------
+    *                             Constants
+    */
+
+
+    /*----------------------------------------------------------------------------
+    *                             Private Variables
+    */
+
+
+    /*------------------------------------------------------------------------
+     *                      PRIVATE METHOD DECLARATIONS
+     */
+    let byIdOrTitle;
+    let bySearch;
+
+    /*----------------------------------------------------------------------------
+    *                             Private Methods
+    */
+    byIdOrTitle = function () {
+
+    };
+
+    bySearch = function () {
+
+    };
+
+    /*----------------------------------------------------------------------------
+    *                             Public API
+    */
+    return {
+        byIdOrTitle: byIdOrTitle,
+        bySearch: bySearch
+    };
+}());
